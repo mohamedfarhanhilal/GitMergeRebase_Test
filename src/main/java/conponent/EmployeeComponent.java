0130a0ef7e14463c3e9861e5df8fed73cf7b8f13
farhan <mdfarhan17@gmail.com>
2023-02-05 14:03:01 +0530

    develop - merge1 Commit - employee name added

--- src/main/java/conponent/EmployeeComponent.java
+++ src/main/java/conponent/EmployeeComponent.java
@@ -1,18 +1,26 @@
 package conponent;
 
 import com.example.demo.Runner;
+import model.Employee;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
 @Service
 public class EmployeeComponent {
+
+
     private static final Logger logger = LogManager.getLogger(EmployeeComponent.class);
 
     public void employeeWelocme(String name) {
-        logger.info("Greeting {}}!",name);
+        logger.info("Greeting {}!",name);
         logger.info("Hello Employee!");
+//        develop
+        Employee emp = new Employee();
+        emp.setName(name);
+        logger.info("Employee added to the pojo {}!",emp.toString());
 
     }
 }

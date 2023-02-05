24460bd8b705f5a838e5abe4f7616add6cbd79d6
farhan <mdfarhan17@gmail.com>
2023-02-05 14:05:44 +0530

    feature - merge1 Commit - employee name check added

--- src/main/java/conponent/EmployeeComponent.java
+++ src/main/java/conponent/EmployeeComponent.java
@@ -1,18 +1,27 @@
 package conponent;
 
 import com.example.demo.Runner;
+import model.Employee;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 
 @Service
 public class EmployeeComponent {
     private static final Logger logger = LogManager.getLogger(EmployeeComponent.class);
 
+    @Autowired
+    Employee employee;
     public void employeeWelocme(String name) {
         logger.info("Greeting {}}!",name);
         logger.info("Hello Employee!");
+
+        if (StringUtils.isEmpty(employee.getName())) {
+            logger.info("Object empty");
+        }
 
     }
 }
